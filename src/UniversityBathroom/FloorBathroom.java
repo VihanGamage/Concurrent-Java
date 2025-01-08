349f32aec62ead10613a2a7535d59ba4ff044e68
VihanGamage <vihanchandula@gmail.com>
2025-01-08 23:20:58 +0530

    fixed scenario3

--- src/UniversityBathroom/FloorBathroom.java
+++ src/UniversityBathroom/FloorBathroom.java
@@ -5,29 +5,29 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
 public class FloorBathroom {
-    private static final int BATHROOM_STALLS = 6;
-    private static final int NUM_EMPLOYEES = 100;
+    private static final int BathroomStalls = 6;
+    private static final int NumOfEmployees = 100;
 
     public static void main(String[] args) {
 
-        Semaphore stalls = new Semaphore(BATHROOM_STALLS);
+        Semaphore stalls = new Semaphore(BathroomStalls);
 
         // Use an ExecutorService to simulate 100 users
-        ExecutorService executor = Executors.newFixedThreadPool(NUM_EMPLOYEES);
+        ExecutorService executor = Executors.newFixedThreadPool(NumOfEmployees);
 
-        for (int i = 1; i <= NUM_EMPLOYEES; i++) {
+        for (int i = 1; i <= NumOfEmployees; i++) {
             int employeeId = i;
-            executor.execute(() -> useBathroom(employeeId, stalls));
+            executor.execute(() -> usingBathroom(employeeId, stalls));
         }
         executor.shutdown();
     }
 
-    private static void useBathroom(int employeeId, Semaphore stalls) {
+    private static void usingBathroom(int employeeId, Semaphore stalls) {
         try {
             System.out.println("Employee "+employeeId+" is waiting to use a stall");
             stalls.acquire(); // Acquire a stall
-            System.out.println("Employee "+employeeId+" is using a stall");
-            Thread.sleep(500);     // Simulate the time taken to use the stall
+            System.out.println("Employee "+employeeId+" is using a bathroom stall");
+            Thread.sleep(500);     //time taken to use the stall
             System.out.println("Employee "+employeeId+" has finished using the stall");
 
         } catch (InterruptedException e) {
